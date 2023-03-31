6ea31df112e6dff5621ecbd529d590202754eb5f
himprover <hwang.improver@gmail.com>
2023-03-31 16:07:21 +0900

    fix: import baseurl [main]

--- webpack.common.js
+++ webpack.common.js
@@ -1,39 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = {
-	entry: './src/index.tsx',
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: ['babel-loader', 'ts-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/,
-				use: [
-					{
-						loader: 'file-loader',
-					},
-				],
-			},
-		],
-	},
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-		}),
-		new DotenvWebpackPlugin(),
-	],
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new DotenvWebpackPlugin(),
+  ],
 };
